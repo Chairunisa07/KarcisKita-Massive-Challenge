e5b55fe2190ed6b55734e79cbec581d81656939c
Chairunisa07 <chairunisaap1@gmail.com>
2024-06-02 13:48:48 +0700

    final admin

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./admin/page/login";
+import Dashboard from "./admin/page/dashboard";
+import Event from "./admin/page/event";
+import DetailEvent from "./admin/page/detail-event";
+import Participant from "./admin/page/participant";
+import Voucher from "./admin/page/voucher";
+import Cash from "./admin/page/cash";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="KarcisKita">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/event" element={<Event />} />
+          <Route path="/detail-event" element={<DetailEvent />} />
+          <Route path="/participant" element={<Participant />} />
+          <Route path="/voucher" element={<Voucher />} />
+          <Route path="/cash" element={<Cash />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
